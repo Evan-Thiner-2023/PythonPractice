a9c8a2b44666aadb59c19ae58115336412ce6d0e
Evan-Thiner-2023 <817548@mystma.org>
2021-09-14 09:43:06 -0500

    top 5 movies coded in python

---
+++ Main.py
@@ -0,0 +1,19 @@
+def topFiveFilms():
+
+    #print 1st movie
+    print("lord of the rings")
+
+    #print 2nd movie
+    print("the hunger games")
+
+    #print 3rd movie
+    print("300")
+
+    #print 4th movie
+    print("the avengers endgame")
+
+    #print 5th movie
+    print("star wars")
+
+print("these are my top 5 movies")
+topFiveFilms()
