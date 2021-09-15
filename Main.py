4e5fd869d3cdef73e7520d1d4af208fba970c3c9
Evan-Thiner-2023 <817548@mystma.org>
2021-09-15 09:14:02 -0500

    coding

--- Main.py
+++ Main.py
@@ -16,4 +16,45 @@ def topFiveFilms():
     print("star wars")
 
 print("these are my top 5 movies")
+#run function
 topFiveFilms()
+def topFiveSports():
+    #print first sport
+    print("soccer")
+    #print second sport
+    print("Football")
+    #print third sport
+    print("rock climbing")
+    #print fourth sport
+    print("skiing")
+    #print fifth sport
+    print("hockey")
+
+def topFiveFamilyMembers():
+    #print 1st family member
+    print("brother")
+    #print 2nd family member
+    print("sister")
+    #print 3rd family member
+    print("mother")
+    #print 4th family member
+    print("father")
+    #print 5th family member
+    print("dog")
+def topFiveVideoGames():
+    print("minecraft")
+    print("assasins creed")
+    print("rocket luege")
+    print("starwars battlefront")
+    print("NHL 2020")
+def topFiveResurants():
+    #print 1st Resurant
+    print("taco bell")
+    #print 2nd Resurant
+    print("jimmy johns")
+    #print 3rd Resurant
+    print("subway")
+    #print 4th Resurant
+    print("chipotle")
+    #print 5th Resurant
+    print("Kains")
