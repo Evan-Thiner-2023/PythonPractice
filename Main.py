b7f6ce84772aaa14ca270d552fabcee16742a25b
Evan-Thiner-2023 <817548@mystma.org>
2021-09-20 09:14:17 -0500

    Last

--- Main.py
+++ Main.py
@@ -1,93 +1,16 @@
-def topFiveFilms():
-    #print 1st movie
-    print("lord of the rings")
-    #print 2nd movie
-    print("the hunger games")
-    #print 3rd movie
-    print("300")
-    #print 4th movie
-    print("the avengers endgame")
-    #print 5th movie
-    print("star wars")
-#run function
-def topFiveSports():
-    #print first sport
-    print("soccer")
-    #print second sport
-    print("Football")
-    #print third sport
-    print("rock climbing")
-    #print fourth sport
-    print("skiing")
-    #print fifth sport
-    print("hockey")
-def topFiveFamilyMembers():
-    #print 1st family member
-    print("brother")
-    #print 2nd family member
-    print("sister")
-    #print 3rd family member
-    print("mother")
-    #print 4th family member
-    print("father")
-    #print 5th family member
-    print("dog")
-def topFiveVideoGames():
-    print("minecraft")
-    print("assasins creed")
-    print("rocket luege")
-    print("starwars battlefront")
-    print("NHL 2020")
-def topFiveResurants():
-    #print 1st Resurant
-    print("taco bell")
-    #print 2nd Resurant
-    print("jimmy johns")
-    #print 3rd Resurant
-    print("subway")
-    #print 4th Resurant
-    print("chipotle")
-    #print 5th Resurant
-    print("Kains")
-#create a function that prints your name
-#followed by "is my name"
-def myNameIs(Turtle):
-    #output string
-    print(Turtle + " is my name")
-#run function
-myNameIs("Evan")
-myNameIs("fhf")
-
-def timesTwo(lol):
-    #*2
-    print(lol*2)
-#run function
-timesTwo(8)
-def minusFive(xx):
-    #minus 5
-    print(xx-5)
-def divideByTwo(yy):
-    #divide by 2
-    print(yy/2)
-#run function
-minusFive(55)
-#run function
-divideByTwo(200)
-def addTwoNumbers(x,y):
-    #add x and y
-    print(x+y)
-#run Function 
-addTwoNumbers(43,34)
-def madLib(a,b,c,d,e,f,g,h,i,j,k):
-    #print madlib
-    print("Star war is a "+ a+ " " + b+ " " + "of " + c +" " + "versus evil in a " + d + " far far away." + " There are " + f + " battles between "+g+" "+h+" "+ i + " in "+ j+ " space and "+ k +" duels with"+ e +" called"+ " sabers")
-#Run madlib
-madLib( "cool","movie","good","galaxy","Epic","cool","space ships","scary","epic","lights","light")
-def dillyDilly(fjf,djd):
-    print(fjf + djd)
-dillyDilly("t","i")
-def geom(loo):
-    print("Okay now this is epic")
-    print(loo)
-geom("cool")
-property(float)
+#define function
+def woof():
+    #return woof
+    return "woof"
+#print variable woof
+print (woof())
+#define function
+def cat():
+    #set value of cat
+    return "Dog"
+#define function
+def dog():
+    #set value of dog
+    return "cat"
+#print to values
+print (cat()+dog())
