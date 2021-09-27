ebfebe99e6a559184d2bd9467dc8adcf68205a4d
Evan-Thiner-2023 <817548@mystma.org>
2021-09-27 09:47:05 -0500

    IDK

--- Main.py
+++ Main.py
@@ -1,6 +1,25 @@
-def greaterThan10(x):
-    if x>10:
-        return "x is greater than 10."
+def define(word):
+    word=word.lower()
+    if word=="airborn":
+        return "to be born in the air"
+    elif word=="stop":
+        return "to become still?"
+    elif word=="sport":
+        return "a physical activity"
+    elif word=="dark":
+        return "hard to see"
+    elif word=="gamer":
+        return "someone who plays video games"
+    elif word=="hard":
+        return "something that is sturdy"
+    elif word=="work":
+        return "something that you have to do to earn money"
+    elif word=="swim":
+        return "to push off water in order to move"
+    elif word=="camoflage":
+        return "to blend into surroundings"
+    elif word=="repeatition":
+        return "to repeat cosistantly"
     else:
-        return "x is not less than 10."
-print(greaterThan10(22))
+        return "definintion not available"
+print(define(""))
